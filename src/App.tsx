c675a6935d4b0a00197a36dec5fe2e815eec3e2c
Martin Joakim Ulstein <6736558+mjulstein@users.noreply.github.com>
2020-03-09 15:05:34 +0100

    init basic TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React typescript base</h1>
+      </header>
+      <main>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default App;
